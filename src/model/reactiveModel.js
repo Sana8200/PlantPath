0b3447d747e4cda2f091a36ef0337d1718953256
Sana Monhaseri <s.monhaserii@gmail.com>
2025-12-22 18:07:42 +0100

    feat: start from scratch, buidling the model

--- src/model/reactiveModel.js
+++
@@ -1,8 +0,0 @@
-import { metaDataModel } from "/src/model/metaDataModel.js";
-import { leafKeeperModel } from "/src/model/model.js";
-import { connectToPersistence } from "/src/model/firebaseModel.js";
-
-export const reactiveLeafKeeperModel = leafKeeperModel;
-export const reactiveMetaDataModel = metaDataModel;
-
-connectToPersistence(reactiveLeafKeeperModel, reactiveMetaDataModel);

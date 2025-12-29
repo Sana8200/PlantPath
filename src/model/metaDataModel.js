d7926decc19b0088baf3cf3da015b56fcf427d0b
Sana Monhaseri <s.monhaserii@gmail.com>
2025-12-29 12:20:34 +0100

    feat: added funciton, completing the metaData model

--- src/model/metaDataModel.js
+++ src/model/metaDataModel.js
@@ -12,4 +12,40 @@ class MetaDataModel{
     makeAutoObservable(this);
   }
 
+  setReadySate(state){
+    this.ready = state;
+  }
+
+  setSideEffectState(state){
+    this.sideEffectSetup = state;
+  }
+
+   /**
+   * INIT FUNCTION:
+   * Sets a comment object array to its key in the dictionary.
+   * @param {String} key The plant ID.
+   * @param {*} commentArr The comment object array to add.
+   */
+  setCommentFieldInit(key, commentArr) {
+    const result = {};
+    for (const user in commentArr) {
+      result[user] = [];
+      for (let i = 0; i < commentArr[user].length; i++) {
+        const obj = {
+          id: commentArr[user][i].id,
+          plantID: commentArr[user][i].plantID,
+          comment: commentArr[user][i].comment,
+          name: commentArr[user][i].name,
+          personID: commentArr[user][i].personID,
+          date: new Date(
+            commentArr[user][i].date.seconds * 1000 +
+            commentArr[user][i].date.nanoseconds / 1000000
+          ),
+        };
+        result[user].push(obj);
+      }
+    }
+    this.comments[key] = result;
+  }
+
 }

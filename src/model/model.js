99cd4cc1adffecedd6cd328a70c3721e808cc74a
Sana Monhaseri <s.monhaserii@gmail.com>
2025-12-30 16:49:04 +0100

    feat: plantPath base model impelmentation, still in progress

--- src/model/model.js
+++ src/model/model.js
@@ -23,4 +23,63 @@ class plantPathModel{
     this.user = user;
   }
 
+  setReadyState(ready) {
+    this.ready = ready;
+  }
+
+  setSESUState(state) {
+    this.sideEffectsSetUp = state;
+  }
+
+  // Plant Selection
+  setCurrentPlant(plant) {
+    this.currentPlant = plant;
+  }
+
+  // Search functions
+  setCurrentQuery(query) {
+    if (typeof query !== 'string') {
+      this.searchError = "Invalid input type";
+      return;
+    }
+    this.searchError = null;
+    this.currentQuery = query;
+  }
+
+  doSearchByQuery() {
+    const trimmedQuery = this.currentQuery.trim();
+    if (!trimmedQuery) {
+      this.searchError = "Please enter a plant name to search.";
+      return;
+    }
+    if (trimmedQuery.length < 2) {   // minimum length like 2 for huge result sets ?! We can ignore it if it's not ok 
+      this.searchError = "Search query is too short.";
+      return;
+    }
+    const promise = doPlantSearchByQuery(trimmedQuery);
+    resolvePromise(promise, this.searchResultsPromiseState);
+  }
+
+
+  doSearchByCategory(catagory) {
+    if (!catagory) {
+      this.searchResults = [];
+      return;
+    }
+    resolvePromise(
+      doPlantSearchByCategory(catagory),
+      this.searchResultsPromiseState
+    );
+  }
+
+
+  loadCategories() {
+    resolvePromise(getAllCategories(), this.searchResultsPromiseState);
+  }
+
+
+
+
+  
+
 }

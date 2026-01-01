63dd5f335f2e32570eaf9735e491d920cc1b6025
Sana Monhaseri <s.monhaserii@gmail.com>
2026-01-01 11:26:49 +0100

    feat: processing the data we get, returning it or catching the error

--- src/api/resolvePromise.js
+++ src/api/resolvePromise.js
@@ -1,21 +1,28 @@
+import { runInAction } from "mobx";
+
 export function resolvePromise(prms, promiseState) {
     function dataACB(response) {
-        //console.log("Resolving promise: ", prms, promiseState.promise, prms == promiseState.promise);
         if (promiseState.promise == prms) {
-            promiseState.data = response;
-            console.log("LOG: ", response, {... {... promiseState.data}[0]}, promiseState.data == response);
+            runInAction(() => {
+                promiseState.data = response;
+            });
         }
     }
     function errorACB(error) {
-        promiseState.error = error;
+        runInAction(() => {
+            promiseState.error = error;
+        });
         if (promiseState.promise && !promiseState.data) {
             console.error("ERROR OCCURED:", error);
         }
     }
-    promiseState.promise = prms;
-    promiseState.data = null;
-    promiseState.error = null;
-    //console.log("Resolving promise: ", prms, promiseState.promise, prms == promiseState.promise);
+
+    runInAction(() => {
+        promiseState.promise = prms;
+        promiseState.data = null;
+        promiseState.error = null;
+    });
+
     if (prms) {
         prms.then(dataACB).catch(errorACB);
     }

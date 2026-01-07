226e45ea5b54ab0d27da8bd3718958990c582d01
Sana Monhaseri <s.monhaserii@gmail.com>
2026-01-07 17:14:00 +0100

    feat: modified model name, changed from old name to the new one

--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -1,17 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { reactiveMetaDataModel, reactiveLeafKeeperModel} from "/src/model/reactiveModel.js";
+import { reactiveMetaDataModel, reactivePlantPathModel} from "/src/model/reactiveModel.js";
 import { ReactRoot } from "/src/reactjs/ReactRoot.jsx";
 
-
 // debugging
-window.myModel = reactiveLeafKeeperModel;
+window.myModel = reactivePlantPathModel;
 window.myMetaDataModel = reactiveMetaDataModel;
 
 console.log("✅ SUCCESS: Model has been attached to window.myModel"); //debugging
 const mountedApp = createRoot(document.getElementById("root"));
 mountedApp.render(
   <ReactRoot
-    leafKeeperModel={reactiveLeafKeeperModel}
+    plantPathModel={reactivePlantPathModel}
     metaDataModel={reactiveMetaDataModel}
   />
 );

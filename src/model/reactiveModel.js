ae0a05138b673df97e34ff3325e4645313ef9038
Sana Monhaseri <s.monhaserii@gmail.com>
2026-01-07 17:14:17 +0100

    feat: changed names

--- src/model/reactiveModel.js
+++ src/model/reactiveModel.js
@@ -1,8 +1,8 @@
 import { metaDataModel } from "/src/model/metaDataModel.js";
-import { PlantPathModel } from "/src/model/model.js";
+import { plantPathModel } from "/src/model/model.js";
 import { connectToPersistence } from "/src/model/firebaseModel.js";
 
-export const reactivePlantPathModel = PlantPathModel;
+export const reactivePlantPathModel = plantPathModel;
 export const reactiveMetaDataModel = metaDataModel;
 
 connectToPersistence(reactivePlantPathModel, reactiveMetaDataModel);

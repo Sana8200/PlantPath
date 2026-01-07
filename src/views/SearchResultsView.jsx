a2e3c7893db64c988a247800a6e6607ef537b7e8
Sana Monhaseri <s.monhaserii@gmail.com>
2026-01-07 17:04:09 +0100

    feat: added css classnames and completed the view

--- src/views/SearchResultsView.jsx
+++ src/views/SearchResultsView.jsx
@@ -1,38 +1,39 @@
-import "/src/style/searchResults.css";
+import { PlantCard } from "../components/PlantCard";
+import "/src/style/search.css";
 
 export function SearchResultsView(props) {
-
-  function handlePlantClick(plant) {
-    props.onPlantSelect(plant);
+  if (!props.searchResults || Object.values(props.searchResults).length === 0) {
+    return (
+      <div className="search-no-results">
+        <p>
+          Oops! No search results were found. Try searching for "Monstera" or "Fern"! 🌱
+        </p>
+      </div>
+    );
   }
 
   function renderPlantCB(plant) {
     return (
       <div
-        key={plant}
-        className="plant-card"
-        onClick={() => handlePlantClick(plant)}
+        key={plant.id}
+        className="plant-card-wrapper"
+        onClick={() => props.onPlantSelect(plant)}
       >
-        <div className="plant-card-name">{plant["Common name (fr.)"]}</div>
-        <img className="plant-card-image"
-          src={plant.Img}
-          height={100}
+        <PlantCard
+          plant={plant}
+          showAddToCollection={true}
+          userCollections={props.userCollections}
+          onAddToCollectionFunction={props.onAddToCollection}
+          showSunDetails={true}
+          showWatering={true}
         />
-        <div className="plant-card-info">
-          <strong className="plant-card-light">Sun instructions:</strong>
-          {plant["Light ideal"]}
-        </div>
-        <select>
-          <option>Choose where to add plant</option>
-          <option>{"Add to favorites <3"}</option>
-          <option>{"Add to my collection 1 🌿"}</option>
-          <option>{"Add to my collection 2 🌿"}</option>
-        </select>
       </div>
     );
   }
 
-  return <div className="search-results-grid">{(props.searchResults || []).map(renderPlantCB)}</div>;
+  return (
+    <div className="search-results-grid">
+      {(Object.values(props.searchResults) || []).map(renderPlantCB)}
+    </div>
+  );
 }
-
-
